41650cce3629d25b61ee36781800d6bcb87e6698
WorkBuddy <workbuddy@local>
2026-08-21 22:06:39 +0800

    deploy: screener/ from static_dist_pub_pub

--- screener/sw.js
+++ screener/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "2026-08-21 04:35:34";
+const SW_VERSION = "2026-08-21 22:01:35";
 const CACHE = 'screener-cache';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
