0dba378d37fd381156b6513a7a8543599d2fe154
WorkBuddy <workbuddy@local>
2026-09-24 07:14:57 +0800

    deploy: screener/ from static_dist_pub_pub

--- screener/sw.js
+++ screener/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = "2026-09-24 03:59:15";
+const SW_VERSION = "2026-09-24 07:14:48";
 const CACHE = 'screener-cache';
 const NETWORK_FIRST = [/data\.json/, /sector_hist\.json/, /index\.html?$/, /\/screener\/$/];
 self.addEventListener('install', e => self.skipWaiting());
